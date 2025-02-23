87c1657756bb7510ca727da9a5e4093c9bd6ee7c
AKHIL-DyC <akhiljee666@gmail.com>
2025-02-23 22:22:16 +0530

    prod fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,6 @@ import NextAuth from "next-auth";
 import Next_AUTH_CONFIG from "@/app/lib/auth"
 
 
-secret: process.env.NEXTAUTH_SECRET;
+
 const handler = NextAuth(Next_AUTH_CONFIG)
 export { handler as GET, handler as POST };
